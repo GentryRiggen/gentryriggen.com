a07658104b4847f328dd388aed12390cf43c2da0
Gentry Riggen <gentryriggen@outlook.com>
2015-08-11 13:52:37 -0600

    Added JWT decoding

--- server/services/jwt.js
+++ server/services/jwt.js
@@ -19,10 +19,38 @@ exports.encode = function(payload) {
   return jwt + '.' + signature;
 };
 
+exports.decode = function(token) {
+  var segments = token.split('.');
+  if (segments.length !== 3) {
+    throw new Error('Token structure incorrect');
+  }
+
+  var header = JSON.parse(base64Decode(segments[0]));
+  var payload = JSON.parse(base64Decode(segments[1]));
+
+  var rawSignature = segments[0] + '.' + segments[1];
+  if (!verify(rawSignature, segments[2], conf.jwt.secret)) {
+    throw new Error('JWT Signature Verification Failed!');
+  }
+
+  return {
+    header: header,
+    payload: payload
+  };
+};
+
 function sign(str, key) {
   return crypto.createHmac('sha256', key).update(str).digest('base64');
 }
 
 function base64Encode(str) {
   return new Buffer(str).toString('base64');
+}
+
+function base64Decode(str) {
+  return new Buffer(str, 'base64').toString();
+}
+
+function verify(rawSignature, signature, secret) {
+  return signature == sign(rawSignature, secret);
 }
