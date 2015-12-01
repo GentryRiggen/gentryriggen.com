33cacb86f6032be5d09db9ad5b9036b1988ce975
Gentry Riggen <GentryRiggen@outlook.com>
2015-12-01 11:09:29 -0700

    Debugging
    Debugging

--- client/src/app/health/health.ctrl.js
+++ client/src/app/health/health.ctrl.js
@@ -18,6 +18,8 @@
           HealthCtrl.selectedDay = resp.data[resp.data.length - 1];
           HealthCtrl.data = resp.data;
           HealthCtrl.loading = false;
+          console.log('Steps Taken %:', Math.round(((HealthCtrl.selectedDay.stepsTaken ? HealthCtrl.selectedDay.stepsTaken : 0) / 9000) * 100));
+          console.log('Calories Burned %:', Math.round(((HealthCtrl.selectedDay.caloriesBurned ? HealthCtrl.selectedDay.caloriesBurned : 0) / 3000) * 100));
         });
     }
 
