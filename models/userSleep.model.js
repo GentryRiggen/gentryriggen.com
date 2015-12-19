857bcf0f0c9b024c6d3cf48aa10ec4f7746aadf8
Gentry Riggen <gentryriggen@outlook.com>
2015-12-18 20:07:51 -0700

    Fixed sleep conversion bug

--- models/userSleep.model.js
+++ models/userSleep.model.js
@@ -33,12 +33,12 @@ exports.fromJson = function (msHealthSleep) {
     number_of_wakeups: msHealthSleep.numberOfWakeups,
     fall_asleep_time: (new Date(msHealthSleep.fallAsleepTime)).toLocalMysqlFormat(),
     wakeup_time: (new Date(msHealthSleep.wakeupTime)).toLocalMysqlFormat(),
-    total_duration: baseMsHealthModel.durationToSeconds(msHealthSleep.duration),
-    awake_duration: baseMsHealthModel.durationToSeconds(msHealthSleep.awakeDuration),
-    sleep_duration: baseMsHealthModel.durationToSeconds(msHealthSleep.sleepDuration),
-    restless_sleep_duration: baseMsHealthModel.durationToSeconds(msHealthSleep.totalRestlessSleepDuration),
-    restfull_sleep_duration: baseMsHealthModel.durationToSeconds(msHealthSleep.totalRestfulSleepDuration),
-    time_to_fall_asleep: baseMsHealthModel.durationToSeconds(msHealthSleep.fallAsleepDuration),
+    total_duration: msHealthSleep.duration ? baseMsHealthModel.durationToSeconds(msHealthSleep.duration) : 0,
+    awake_duration: msHealthSleep.awakeDuration ? baseMsHealthModel.durationToSeconds(msHealthSleep.awakeDuration) : 0,
+    sleep_duration: msHealthSleep.sleepDuration ? baseMsHealthModel.durationToSeconds(msHealthSleep.sleepDuration) : 0,
+    restless_sleep_duration: msHealthSleep.totalRestlessSleepDuration ? baseMsHealthModel.durationToSeconds(msHealthSleep.totalRestlessSleepDuration) : 0,
+    restfull_sleep_duration: msHealthSleep.totalRestfulSleepDuration ? baseMsHealthModel.durationToSeconds(msHealthSleep.totalRestfulSleepDuration) : 0,
+    time_to_fall_asleep: msHealthSleep.fallAsleepDuration ? baseMsHealthModel.durationToSeconds(msHealthSleep.fallAsleepDuration) : 0,
     sleep_efficiency: msHealthSleep.sleepEfficiencyPercentage
   };
 
