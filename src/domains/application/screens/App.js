3ed2a145144e5d44372c6fa4e4e4e084b0664aed
Gentry Riggen <gentryriggen@outlook.com>
2018-12-22 14:44:52 -0700

    Create React App. TypeScript is for the birds

---
+++ src/domains/application/screens/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
