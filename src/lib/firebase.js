31def20a2053cbcfb11d68caf839b0cd9745aa1d
Gentry Riggen <gentryriggen@outlook.com>
2018-12-30 21:10:56 -0700

    Firebase import only used packages

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 import rebase from 're-base';
 import env from 'env';
 
