be7157342fb19da79d9285b54dbc6385212256af
Gentry Riggen <gentryriggen@outlook.com>
2019-01-01 12:21:04 -0700

    FlyIn Animations

--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './Button';
 export { default as Fade } from './Fade';
+export { default as FlyIn } from './FlyIn/FlyIn';
 export { default as Header } from './Header';
 export { default as Icon } from './Icon';
 export { default as Input } from './Input';
