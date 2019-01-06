7ffd696748a8e5a16b3d1b0386862d729c5297bb
Gentry Riggen <gentryriggen@outlook.com>
2019-01-05 21:51:35 -0700

    Running dev server before running test scripts - 4

--- e2e/app.e2e.test.js
+++ e2e/app.e2e.test.js
@@ -1,6 +1,7 @@
 describe('Site launches', () => {
   beforeAll(async () => {
     await page.goto('http://localhost:3000');
+    await page.waitForSelector('[data-test="screen-container"]');
   });
 
   it('should display "Hi, I\'m Gentry" text on page', async () => {
