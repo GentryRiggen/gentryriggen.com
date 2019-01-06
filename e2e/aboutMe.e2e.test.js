830ad79d83b11ad65bba050d27ee2566eedf71f4
Gentry Riggen <gentryriggen@outlook.com>
2019-01-05 22:35:56 -0700

    Running dev server before running test scripts - 12

--- e2e/aboutMe.e2e.test.js
+++ e2e/aboutMe.e2e.test.js
@@ -2,6 +2,7 @@ describe('About Me Page loads properly', () => {
   beforeAll(async () => {
     console.log('IP Address:', require('ip').address());
     await page.goto(`http://${require('ip').address()}:3000/about`);
+    await page.waitFor(100);
     await page.waitForSelector('[data-test="about-me"]');
   });
 
