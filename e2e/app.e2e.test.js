a1f3d257f7fa03a707c999d4134087751a25f003
Gentry Riggen <gentryriggen@outlook.com>
2019-01-05 21:44:09 -0700

    Running dev server before running test scripts

--- e2e/app.e2e.test.js
+++ e2e/app.e2e.test.js
@@ -1,9 +1,9 @@
-describe('Google', () => {
+describe('Site launches', () => {
   beforeAll(async () => {
-    await page.goto('https://google.com');
+    await page.goto('http://localhost:3000');
   });
 
-  it('should display "google" text on page', async () => {
-    await expect(page).toMatch('google');
+  it('should display "Hi, I\'m Gentry" text on page', async () => {
+    await expect(page).toMatch('Hi, I\'m Gentry.');
   });
 });
