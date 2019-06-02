7a08a723e8b80fbd2a8ac61d5a9a3093a019b761
Gentry Riggen <gentryriggen@outlook.com>
2019-06-02 13:30:42 -0600

    fix: removed react-animation

--- src/domains/splash/components/Header.tsx
+++ src/domains/splash/components/Header.tsx
@@ -1,32 +1,18 @@
 import React from 'react'
-import { HideUntilLoaded } from 'react-animation'
 
 import View from 'lib/components/View'
 import Image from 'lib/components/Image'
 import Text from 'lib/components/Text'
-import Spinner from 'lib/components/Spinner'
 
 export const Header = () => {
   return (
-    <View
-      animation="fadeInDown"
-      flexible="row-space-between"
-      height={86}
-    >
+    <View animation="fadeInDown" flexible="row-space-between" height={86}>
       <View flexible="row-v-center">
-        <HideUntilLoaded
-          animationIn="bounceIn"
-          imageToLoad="images/MultiColorLogo.png"
-          Spinner={Spinner}
-        >
-          <Image
-            src="images/MultiColorLogo.png"
-            // @ts-ignore
-            width={[32, 50]}
-            // @ts-ignore
-            height={[32, 50]}
-          />
-        </HideUntilLoaded>
+        <Image
+          src="images/MultiColorLogo.png"
+          width={[32, 50]}
+          height={[32, 50]}
+        />
         <Text fontSize={[18, 24]} fontWeight={700} ml={[2, 3]}>
           gentry
         </Text>
